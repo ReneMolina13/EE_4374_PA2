c04640fc5d55ef2204f66201395e25d4b6b23f32
Rene Molina <rdmolina@miners.utep.edu>
2023-02-09 18:35:16 -0700

    Update molina2.c

--- molina2.c
+++ molina2.c
@@ -68,7 +68,7 @@ void *catalanThread(void *param)
 		
 		// c(n) = temp_result / n!
 		catalan = temp_result / factorials[n];
-		fprintf(catalan_file, "n = %d, c(n) = %llf\n", n, catalan);
+		fprintf(catalan_file, "n = %d, c(n) = %Lf.0\n", n, catalan);
 	}
 	
 	// Close file and exit thread

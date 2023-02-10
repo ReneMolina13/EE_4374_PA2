2eb4cc8d4c5595ba23d919fca1653698b4995dad
Rene Molina <rdmolina@miners.utep.edu>
2023-02-09 19:17:18 -0700

    Update molina2.c

--- molina2.c
+++ molina2.c
@@ -136,21 +136,24 @@ int main(int argc, char **argv)
 		thread_num_start_end[THREAD_START] = work_assigned + 1;
 		work_assigned += divided_work;
 		thread_num_start_end[THREAD_END] = work_assigned;
-		pthread_create(tid++, &attr, catalanThread, (void *) thread_num_start_end);
+		pthread_create(tid, &attr, catalanThread, (void *) thread_num_start_end);
+		tid++;
 		
 	case 3:
 		thread_num_start_end[THREAD_NUM] = 3;
 		thread_num_start_end[THREAD_START] = work_assigned + 1;
 		work_assigned += divided_work;
 		thread_num_start_end[THREAD_END] = work_assigned;
-		pthread_create(tid++, &attr, catalanThread, (void *) thread_num_start_end);
+		pthread_create(tid, &attr, catalanThread, (void *) thread_num_start_end);
+		tid++;
 		
 	case 2:
 		thread_num_start_end[THREAD_NUM] = 2;
 		thread_num_start_end[THREAD_START] = work_assigned + 1;
 		work_assigned += divided_work;
 		thread_num_start_end[THREAD_END] = work_assigned;
-		pthread_create(tid++, &attr, catalanThread, (void *) thread_num_start_end);
+		pthread_create(tid, &attr, catalanThread, (void *) thread_num_start_end);
+		tid++;
 	
 	case 1:
 		thread_num_start_end[THREAD_NUM] = 1;
